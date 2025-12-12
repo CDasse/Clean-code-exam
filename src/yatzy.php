1c7bdba3555b65122a598bb9273da335b2a57e46
CDasse <clemence.dasse@yahoo.fr>
2025-12-12 16:30:35 +0100

    Refactor: rename cunter i

--- src/yatzy.php
+++ src/yatzy.php
@@ -113,8 +113,8 @@ class Yatzy
     public function fours(): int
     {
         $sum = 0;
-        for ($at = 0; $at !== 5; $at++) {
-            if ($this->dice[$at] === 4) {
+        for ($i = 0; $i < 5; $i++) {
+            if ($this->dice[$i] === 4) {
                 $sum += 4;
             }
         }
